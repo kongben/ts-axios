e94ab9954a04ac0f73186c79acd487a4a096065d
kongben <1317495859@qq.com>
2019-07-19 18:19:25 +0800

    add interce

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,5 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
-import xhr from './xhr'
-import { buildURL } from './helpers/url'
-import { transformRequest, transformResponse } from './helpers/data'
-import { processHeaders } from './helpers/headers'
-function axios(config: AxiosRequestConfig): AxiosPromise {
-    processConfig(config)
-    return xhr(config).then((res) => {
-        return transformResponseData(res)
-    })
-}
+import axios from './axios'
 
-function processConfig(config: AxiosRequestConfig): void {
-    config.url = transformURL(config)
-    config.headers = transformHeaders(config)
-    config.data = transformRequestData(config)
-}
+export * from './types'
 
-function transformURL(config: AxiosRequestConfig): string {
-    const { url, params } = config
-    return buildURL(url, params)
-}
-function transformRequestData(config: AxiosRequestConfig): any {
-    return transformRequest(config.data)
-
-}
-
-function transformHeaders(config: AxiosRequestConfig): any {
-    const { headers = {}, data } = config
-    return processHeaders(headers, data)
-}
-
-function transformResponseData(res: AxiosResponse): AxiosResponse {
-    res.data = transformResponse(res.data)
-    return res
-}
-export default axios 
+export default axios
